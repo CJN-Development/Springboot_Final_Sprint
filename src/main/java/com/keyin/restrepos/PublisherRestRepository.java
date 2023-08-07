35e2eb061bec917ac65859829bef3013c09228ff
DevinAugot <105358840+DevinAugot@users.noreply.github.com>
2023-08-07 11:42:45 -0230

    Removed Constructor
    Removed Constructor was causing issues with postman

--- src/main/java/com/keyin/restrepos/PublisherRestRepository.java
+++ src/main/java/com/keyin/restrepos/PublisherRestRepository.java
@@ -1,12 +1,12 @@
 package com.keyin.restrepos;
 
-
 import com.keyin.entities.Publisher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
+
 
 @RepositoryRestResource(collectionResourceRel = "publisher", path = "publisher")
 public interface PublisherRestRepository extends JpaRepository<Publisher, Long> {
 //    List<Game> findByPublisherByName(Publisher publisher);
+        Publisher findByPublisherName(String publisherName);
 }
