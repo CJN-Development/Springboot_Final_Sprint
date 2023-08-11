deed352e7c28036c46e53fdbb18a8ab5b8a285be
Nathan Greene <nathan.greene@keyin.com>
2023-08-10 22:10:40 -0230

    added basic auth for postman

---
+++ src/main/java/com/keyin/security/MessageController.java
@@ -0,0 +1,12 @@
+package com.keyin.security;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "User/Pass Accepted!";
+    }
+}
